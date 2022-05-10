bd45ee83425dcdeea460aec1ed959a668930a739
vladyslovestepanov <vstepanov@neonomics.io>
2022-05-10 13:17:59 +0300

    added migration file

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Adoption {
     address [16] public adopters;
 
-    function getAdopters() public view returns (address[16]) {
+    function getAdopters() public view returns ( address[16] memory) {
         return adopters;
     }
 
-    function adopt(uint petId) public retruns (uint) {
+    function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
         adopters[petId] = msg.sender;
         return petId;
